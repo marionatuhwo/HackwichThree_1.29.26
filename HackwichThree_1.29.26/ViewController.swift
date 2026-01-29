235250f66224794e0a0618dec25048ae5cc7f43b
marionatuhwo <ano@hawaii.edu>
2026-01-29 12:19:10 -1000

    Completed Part 3 of Hackwich 3 In class practice checklist, still need to complete part 5

--- HackwichThree_1.29.26/ViewController.swift
+++ HackwichThree_1.29.26/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var firstString = "The background color will turn blue"
+    var secondString = "The background color will turn green"
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeColorButtonPressed(_ sender: Any)
+    {
+        
+        if firstString == "The background will turn red"
+               {
+                   self.view.backgroundColor = UIColor.red
+               }
+               else
+               {
+                   self.view.backgroundColor = UIColor.blue
+               }
 
+    }
+    
 }
 
